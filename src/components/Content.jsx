ca95d2c74cbbb6e9aff5878def139f52cb770bc1
magnol1a <beaniesmellsgoodsometimes@seriouscookie.lol>
2026-09-13 16:08:06 +1000

    moving cookie text

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,13 +1,78 @@
+import React, { useRef, useState } from "react";
+
 function Content() {
+    const mousePosition = useMousePosition();
+
+    const cookieRef = useRef(null);
+    const [rotation, setRotation] = useState({x: 0, y: 0});
+
+    const handleMouseMove = (e) => {
+        
+        if (!cookieRef.current) return;
+
+        const rect = cookieRef.current.getBoundingClientRect()
+
+        // rectangle position from left of screen plus half of width
+        const centerX = rect.left + rect.width / 2;
+        // rectangle position from top of screen plus half of height
+        const centerY = rect.top + rect.height / 2;
+
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+
+        const deltaX = mouseX - centerX;
+        const deltaY = mouseY - centerY;
+        
+        setRotation({x: deltaX, y: -deltaY})
+    }
+
+    const handleMouseLeave = () => {
+        setRotation({x: 0, y: 0})
+    }
     return (
 
         <div className = "content">
             <main>
-                <h2 className="medium-text">welcome to serious<span className="cookie">cookie</span></h2>
-                <p className="extra">gu</p>
+                <h2 className="medium-text">welcome to serious
+                    <span ref={cookieRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
+                        <span 
+                            className="cookie"
+                            style={{
+                                display: "inline-block",  
+                                cursor: 'default',
+                                transform: `rotate3d(${rotation.y}, ${rotation.x}, 0, 40deg)`,
+                                transition: 'transform 0.5s ease-out'
+                            }}
+                        >
+                                cookie
+                            </span>
+                    </span>
+                </h2>
+                <p className="extra">{JSON.stringify(mousePosition)}</p>
             </main>
         </div>
     )
 }
+
+const useMousePosition = () => {
+  const [
+    mousePosition,
+    setMousePosition
+  ] = React.useState({ x: null, y: null });
+
+  React.useEffect(() => {
+    const updateMousePosition = ev => {
+      setMousePosition({ x: ev.clientX, y: ev.clientY });
+    };
+    
+    window.addEventListener('mousemove', updateMousePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, []);
+
+  return mousePosition;
+};
 
 export default Content
